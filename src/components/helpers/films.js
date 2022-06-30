2c81ddf3bdedfc859a6d959b2940349d74b38bcb
CameronSuperrb <cameron@superrb.com>
2022-06-30 12:53:43 +0100

    update exports to ES syntax

--- src/components/helpers/films.js
+++ src/components/helpers/films.js
@@ -1,16 +1,14 @@
-module.exports = {
-  getTrendingFilms: async function (timeframe) {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/${timeframe}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-    )
-    const data = await response.json()
-    return data
-  },
-  getFilmById: async function (id) {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-    )
-    const data = await response.json()
-    return data
-  },
+export async function getTrendingFilms(timeframe) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/${timeframe}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  )
+  const data = await response.json()
+  return data
+}
+export async function getFilmById(id) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  )
+  const data = await response.json()
+  return data
 }
--- src/components/helpers/search.js
+++ src/components/helpers/search.js
@@ -1,9 +1,7 @@
-module.exports = {
-  search: async function (term, page) {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${term}&page=${page}&include_adult=false`
-    )
-    const data = await response.json()
-    return data
-  },
+export async function search(term, page) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${term}&page=${page}&include_adult=false`
+  )
+  const data = await response.json()
+  return data
 }
