fd33315fc5f4d55627a45017079ffc96614dd11b
CameronSuperrb <cameron@superrb.com>
2022-07-03 21:11:25 +0100

    add isVisible hook to film component

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,5 +1,7 @@
+import { useRef } from 'react'
 import { FaMinus, FaPlus, FaQuestionCircle, FaStar } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import useOnScreen from '../hooks/on-screen.js'
 import {
   addFilm,
   addShow,
@@ -8,46 +10,56 @@ import {
 } from './helpers/localStorage.js'
 
 const Film = ({ film, config, isInList, type }) => {
+  const ref = useRef()
+  const isVisible = useOnScreen(ref)
+
   return (
-    <div className="objFit film__container" aria-label={film.original_title}>
-      <Link to={`/movie/${film.id}`} className="film__link">
-        <div className="film__image">
-          {film.poster_path ? (
-            <img
-              src={`${config?.images?.base_url}${config?.images?.profile_sizes[2]}${film?.poster_path}`}
-              alt={`${film.title}`}
-              loading="lazy"
-            />
-          ) : (
-            <div className="film__image--placeholder">
-              <FaQuestionCircle />
-              <h5>{film.title}</h5>
-            </div>
-          )}
-        </div>
-      </Link>
-      <div className="film__details">
-        {film.vote_average > 0 && film.vote_average.toFixed(1) !== 0 && (
-          <div className="film__details--rating">
-            <FaStar />
-            {film?.vote_average.toFixed(1)}
-          </div>
-        )}
+    <div className="objFit film__container" ref={ref}>
+      {isVisible && (
         <div
-          className="film__details--list"
-          onClick={() =>
-            type === 'movie'
-              ? isInList
-                ? removeFilm(film.id)
-                : addFilm(film.id)
-              : isInList
-              ? removeShow(film.id)
-              : addShow(film.id)
-          }
+          className="objFit film__container"
+          aria-label={film.original_title}
         >
-          {isInList ? <FaMinus /> : <FaPlus />}
+          <Link to={`/movie/${film.id}`} className="film__link">
+            <div className="film__image">
+              {film.poster_path ? (
+                <img
+                  src={`${config?.images?.base_url}${config?.images?.profile_sizes[2]}${film?.poster_path}`}
+                  alt={`${film.title}`}
+                  loading="lazy"
+                />
+              ) : (
+                <div className="film__image--placeholder">
+                  <FaQuestionCircle />
+                  <h5>{film.title}</h5>
+                </div>
+              )}
+            </div>
+          </Link>
+          <div className="film__details">
+            {film.vote_average > 0 && film.vote_average.toFixed(1) !== 0 && (
+              <div className="film__details--rating">
+                <FaStar />
+                {film?.vote_average.toFixed(1)}
+              </div>
+            )}
+            <div
+              className="film__details--list"
+              onClick={() =>
+                type === 'movie'
+                  ? isInList
+                    ? removeFilm(film.id)
+                    : addFilm(film.id)
+                  : isInList
+                  ? removeShow(film.id)
+                  : addShow(film.id)
+              }
+            >
+              {isInList ? <FaMinus /> : <FaPlus />}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
